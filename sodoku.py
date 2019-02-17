3c62f7b766ecbaddc62a1e41ed34099ac46a28fb
Marlon Fuentes <marlonfuenteslp@gmail.com>
2019-02-16 21:58:42 -0600

    sudo has column and row check methods

--- sodoku.py
+++ sodoku.py
@@ -1,25 +1,31 @@
+from astar import *
+
+def row(sudoku):
+    listRow=[]
+    for i in sudoku:
+        value=sum(i)
+        if value==10:
+            listRow.append(True)
+        else:
+            listRow.append(False)
+    return listRow
+
+def column(sudoku):
+    listColumn=[]
+    for column in range(len(sudoku[0])):
+        value = 0
+        for row in sudoku:
+            value+=row[column]
+        if value==10:
+            listColumn.append(True)
+        else:
+            listColumn.append(False)
+    return listColumn
+
+
 #cadena a convertir (prueba)
 inputString= ".4.13.4.1..3.21."
 board= [[0 for x in range(4)]for y in range(4)]
 
-def formatList(inputString):
-    exitList=[]
-    for i in inputString:
-        if i==".":
-            exitList.append(0)
-        else:
-            exitList.append(int(i))
-    return exitList
-def crearTablero(board, listSudoku):
-    count=0
-    for i in range(0,4):
-        for j in range(0,4):
-            board[i][j]=listSudoku[count]
-            count+=1
-    return board
-
 listSudoku = formatList(inputString)
 sudoku=crearTablero(board, listSudoku)
-
-print(listSudoku)
-print(sudoku)
